fa14bf739d9aa4af1739390a2bbb94788725070d
Diego <godie8a@hotmail.com>
2022-02-10 20:04:39 -0500

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main -print phrase
+ *
+ */
+
 int main (void)
+
 {
 
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+ 
 }
