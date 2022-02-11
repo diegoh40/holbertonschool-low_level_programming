db174f567381d65e7f14aa6f15fb9203319accb6
Diego <godie8a@hotmail.com>
2022-02-10 20:08:39 -0500

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,8 @@
  * main -print phrase
  * Return: 0
  */
-
 int main (void)
-
 {
-
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
- 
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0); 
 }
