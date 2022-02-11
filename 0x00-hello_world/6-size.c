50f256f581c265df3d4e498a882aa8b76fbf7db1
Diego <godie8a@hotmail.com>
2022-02-10 21:25:33 -0500

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - display size of various types
@@ -18,5 +18,5 @@ int main(void)
     printf("Size of a long long int: %d byte(s)\n", sizeof(n));
     printf("Size of a float: %d byte(s)\n", sizeof(x));
     
-    return 0;
+    return (0);
 }
