ee01ffbec5c9cd994937f97f932d0b59d17b9ffd
Diego <godie8a@hotmail.com>
2022-02-11 21:35:12 -0500

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the alphabet 
+ * Return: Aways 0 (Sucess)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
