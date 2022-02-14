3eff16b694de459f4f3bbcc7d225593d01607a6d
Diego <godie8a@hotmail.com>
2022-02-14 14:42:02 -0500

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+void main ()
+
+{
+  
+   printf("_putchar"\n");
+   
+   
+   return(0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+File 0.Write a program that prints _putchar, followed by a new line.
