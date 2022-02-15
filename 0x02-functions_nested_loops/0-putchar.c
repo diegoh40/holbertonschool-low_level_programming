8351eea061027e4818c3764ac386931bc1d994b0
Diego <godie8a@hotmail.com>
2022-02-15 18:42:38 -0500

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints _putchar, followed by a new line
- * Return: Always 0.
+ * Return: Always 0 (success).
  */
 int main(void)
 {
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * file: main.h
+ * Auth: Diego Hernandez
+ * Desc: Header file declarations for all funtions
+ */
+
+
 int _putchar(char c)
+
+#endif
+  
