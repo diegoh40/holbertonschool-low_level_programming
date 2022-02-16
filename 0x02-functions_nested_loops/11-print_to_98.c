4eaa7af0556712a224f36b3111b4d41c5fcd8214
Diego <godie8a@hotmail.com>
2022-02-15 21:55:06 -0500

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * print_to_98 - prints all natural numbers from n to 98,
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -20,4 +20,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n)
+  
 #endif
+  
