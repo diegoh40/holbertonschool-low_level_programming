7bc292d3e3fe9ba5270fab2774cbc59d4647173c
Diego <godie8a@hotmail.com>
2022-02-16 19:03:52 -0500

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -9,10 +9,8 @@
 void positive_or_negative(int n)
 {
         int n;
-
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        if (n > 0)
+        int n = 0;
+	if (n > 0)
                 printf("%d is positive\n", n);
         else if (n == 0)
                 printf("%d is zero\n", n);
