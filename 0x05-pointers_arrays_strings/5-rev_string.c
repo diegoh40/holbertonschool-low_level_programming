8b90f47ecdbd73e3571c4f0e04aad24f5a3db677
Diego <godie8a@hotmail.com>
2022-02-21 18:03:40 -0500

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,23 +3,22 @@
 
 /**
  * rev_string - reverse string
- *@s: string 
+ * @s: string
  * Return: 0
  */
 void rev_string(char *s)
 {
-	char rev = s[0];
-	int f = 0;
-	int i;
+char rev = s[0];
+int f = 0;
+int i;
+while (s[f] != '\0')
+f++;
 
-	while (s[f] != '\0')
-		f++;
-
-	for (i = 0; i < f; i++)
-	{
-		f--;
-		rev = s[i];
-		s[i] = s[f];
-		s[f] = rev;
-	}
+for (i = 0; i < f; i++)
+{
+f--;
+rev = s[i];
+s[i] = s[f];
+s[f] = rev;
+}
 }
