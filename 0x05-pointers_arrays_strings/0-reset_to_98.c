1b9c175cbaf2172c4b6578dca2500fc125a4c895
Diego <godie8a@hotmail.com>
2022-02-21 14:34:05 -0500

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -11,14 +11,15 @@ void reset_to_98(int *n);
 int n;
 
 n = 402;
-    
+ {    
 printf("n=%d\n", n);
-
+ }
+ 
 reset_to_98(&n);
 *n = 98;
-    
+ {   
 printf("*n=%d\n", *n);
-
+ }
 return (0);
 
 }
