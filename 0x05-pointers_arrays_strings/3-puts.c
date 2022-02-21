ce2a318a5e1e5f81883484f9027e0b9fa1ded55f
Diego <godie8a@hotmail.com>
2022-02-21 16:39:21 -0500

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,14 +2,15 @@
 #include "main.h"
 
 /**
- * _puts - swaps the values of two integers
- *@str: firt integer
+ * _puts - prints a string, followed by a new line
+ *@str: string
  * Return: 0
  */
 void _puts(char *str)
 {
-_putchar("%s\n", str);
-_puts(str);
-return (str);
+while (*str)
+  
+_putchar(*str++);
+_puts('\n');
 
 }
