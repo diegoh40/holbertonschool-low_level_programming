940a9669345abc5a0a578e851c4c10fe51c2433f
Diego <godie8a@hotmail.com>
2022-02-21 16:55:04 -0500

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - print a string in revrse
+ * @*s: String
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+int i = 0;
+
+while (*(s + i))
+i++;
+i = i - 1;
+while (i >= 0)
+{
+_putchar(*(s + i));
+i--;
+}
+_putchar('\n');
+}
