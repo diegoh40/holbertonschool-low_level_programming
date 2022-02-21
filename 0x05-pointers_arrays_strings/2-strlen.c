4225bee214810e91edc12cfe3ffe31df68608d57
Diego <godie8a@hotmail.com>
2022-02-21 16:05:11 -0500

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen- swaps the values of two integers
+ *@a: firt integer
+ *@b: Second integer
+ * Return: 0
+ */
+int _strlen(char *s);
+{
+chr *s;
+int len;
+
+len = _strlen(str);
+
+}
