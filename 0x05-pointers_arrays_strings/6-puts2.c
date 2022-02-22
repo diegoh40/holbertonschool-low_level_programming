161f71e4049e190cb4af60d4d997dfe29cd1b487
Diego <godie8a@hotmail.com>
2022-02-21 19:34:57 -0500

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,11 @@
  * Return: 0
  */
 void puts2(char *str) {
-int index = 0;
-len = 0;
+  int index = 0, len = 0;
 
 while (str[index++])
 len++;
-for(index i = 0; index < len; index += 2)
+for(index = 0; index < len; index += 2)
 _putchar(str[index]);
 	
 _putchar('\n');
