4dbae817651d53c3848693a3ff8c021776a14b2c
Diego <godie8a@hotmail.com>
2022-02-21 19:12:43 -0500

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <main.h>
+
+
+/**
+ * puts - function that prints every other character of a string
+ * @s - string
+ * Retur: 0
+ */
+void puts2(char *str) {
+    *str = "0123456789";
+    char s[40] = "";
+
+    for(int i = 0; i < strlen(p); i+=2){
+        _putchar(p[i]);
+        s[i/2]=p[i];
+    }
+
+    
+    return 0;
+}
