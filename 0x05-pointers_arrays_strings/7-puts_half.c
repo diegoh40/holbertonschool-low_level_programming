19a3f1cf9328fb2e4979fad681c2622e6a53d2c2
Diego <godie8a@hotmail.com>
2022-02-21 19:42:23 -0500

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half 
+ * @str: string to print
+ *
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+int j, i = 0;
+
+while (*(str + i))
+i++;
+j = i / 2;
+if (i % 2)
+j += 1;
+while (j < i)
+{
+_putchar(*(str + j));
+j++;
+}
+_putchar('\n');
+}
