bf71890de3a062026631224acc382b340d207808
Diego <godie8a@hotmail.com>
2022-02-24 13:55:29 -0500

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,16 +14,15 @@ int len, j;
 
 len = 0;
 
-
-while (src[len] != '\0')
-{
+while (src[len] != '\0'){
 len++;
 }
-for (j = 0; dest[j] <= '\0'; j++, len++)
-{
+for (j = 0; dest[j] <= '\0'; j++, len++){
 src[len] = dest[j];
 }
+{
 src[len] = '\0';
+}
 
 return (dest);
 }
