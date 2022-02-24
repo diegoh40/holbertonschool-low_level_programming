4397e98c4e1bea01a6b7ea3421828d22df651a6d
Diego <godie8a@hotmail.com>
2022-02-24 12:32:25 -0500

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ * _strcat - function that concatenates two strings
+ *@len: variable indice
+ *@j: variable indice
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+
+int len, j;
+
+len = 0;
+
+while (src[len] != '\0'){
+len++;
+}
+for (j = 0; dest[j] <= '\0'; j++, len++){
+src[len] = dest[j];
+}
+src[len] = '\0';
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+
+#endif /* MAIN_H */
