7bb45f70f1c456eb54948e58e8146ad2558ff0ec
Diego <godie8a@hotmail.com>
2022-02-24 18:10:26 -0500

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,15 @@
 int _strcmp(char *s1, char *s2)
 
 {
-while( ( *s1 != '\0' && *s2 != '\0' ) && *s1 == *s2 )
-    {
+while(( *s1 != '\0' && *s2 != '\0' ) && *s1 == *s2)
+{
 s1++;
 s2++;
 }
 
 if(*s1 == *s2)
 {
-return 0; // strings are identical
+return 0;
 }
 
 else
